00532f72aa86b1963ee9a9af76d1bff85496eddd
bugbit <obernandezba@gmail.com>
2017-08-04 09:47:38 +0200

    BuilderGLUTNativeDataToManaged

--- OpenTK/Dibuixos/BuilderGLUTNativeDataToManaged/GLUT.cpp
+++ OpenTK/Dibuixos/BuilderGLUTNativeDataToManaged/GLUT.cpp
@@ -14,6 +14,8 @@ void GLUT::CreateAndWriteFonts()
 		auto pFonts = gcnew GLUT::SFG_Fonts();
 
 		CreateFontsBitMap(pFonts);
+		CreateFontStrokeMonoRoman(pFonts);
+		CreateFontStrokeRoman(pFonts);
 
 		auto pXmlSerializer = gcnew System::Xml::Serialization::XmlSerializer(GLUT::SFG_Fonts::typeid);
 		pWriter = gcnew System::IO::StreamWriter(L"fgFonts.xml");
@@ -53,6 +55,57 @@ GLUT::SFG_Font^ GLUT::CreateFont(::SFG_Font *f)
 
 	pFont->xorig = f->xorig;
 	pFont->yorig = f->yorig; /* Relative origin of the character */
+
+	return pFont;
+}
+
+GLUT::SFG_StrokeFont^ GLUT::CreateFont(::SFG_StrokeFont *f)
+{
+	auto pFont = gcnew SFG_StrokeFont();
+	auto l = f->Quantity;
+	auto ptr = f->Characters;
+	auto pCars = gcnew array<SFG_StrokeChar^>(l);
+
+	for (int i = 0; i < l; i++, ptr++)
+	{
+		auto pCar = gcnew SFG_StrokeChar();
+
+		if (*ptr != NULL)
+		{
+			auto l2 = (*ptr)->Number;
+			auto pStrips = gcnew array<SFG_StrokeStrip^>(l2);
+			auto ptr2 = (*ptr)->Strips;
+
+			for (int j = 0; j < l2; j++, ptr2++)
+			{
+				auto pStrip = gcnew SFG_StrokeStrip();
+				auto l3 = ptr2->Number;
+				auto pVexs = gcnew array<SFG_StrokeVertex ^>(l3);
+				auto ptr3 = ptr2->Vertices;
+
+				for (int k = 0; k < l3; k++, ptr3++)
+				{
+					auto pVex = gcnew SFG_StrokeVertex();
+
+					pVex->X = ptr3->X;
+					pVex->Y = ptr3->Y;
+					pVexs[k] = pVex;
+				}
+				pStrip->Number = ptr2->Number;
+				pStrip->Vertices = pVexs;
+				pStrips[j] = pStrip;
+			}
+
+			pCar->Right = (*ptr)->Right;
+			pCar->Number = l;
+			pCar->Strips = pStrips;
+		}
+		pCars[i] = pCar;
+	}
+	pFont->Name = gcnew System::String(f->Name);
+	pFont->Quantity = f->Quantity;
+	pFont->Height = f->Height;
+	pFont->Characters = pCars;
 
 	return pFont;
 }
--- OpenTK/Dibuixos/BuilderGLUTNativeDataToManaged/GLUT.h
+++ OpenTK/Dibuixos/BuilderGLUTNativeDataToManaged/GLUT.h
@@ -16,6 +16,37 @@ public:
 		property float xorig;
 		property float yorig; /* Relative origin of the character */
 	};
+	ref class SFG_StrokeVertex
+	{
+	public:
+		property float X;
+		property float Y;
+	};
+
+	ref class SFG_StrokeStrip
+	{
+	public:
+		property int Number;
+		property array<SFG_StrokeVertex^>^ Vertices;
+	};
+
+	ref class SFG_StrokeChar
+	{
+	public:
+		property float Right;
+		property int Number;
+		property array<SFG_StrokeStrip^>^ Strips;
+	};
+
+	ref class SFG_StrokeFont
+	{
+	public:
+		property System::String^ Name;                       /* The source font name      */
+		property int Quantity;                   /* Number of chars in font   */
+		property float Height;                     /* Height of the characters  */
+		property array<SFG_StrokeChar^>^ Characters;          /* The characters mapping    */
+	};
+
 	ref class SFG_Fonts
 	{
 	public:
@@ -26,6 +57,8 @@ public:
 		property SFG_Font^ FontHelvetica18;
 		property SFG_Font^ FontTimesRoman10;
 		property SFG_Font^ FontTimesRoman24;
+		property SFG_StrokeFont^ StrokeRoman;
+		property SFG_StrokeFont^ StrokeMonoRoman;
 	};
 
 	GLUT();
@@ -34,5 +67,8 @@ public:
 private:
 	static SFG_Font^ CreateFont(::SFG_Font *f);
 	static void CreateFontsBitMap(SFG_Fonts^ pFonts);
+	static SFG_StrokeFont^ CreateFont(::SFG_StrokeFont *f);
+	static void CreateFontStrokeMonoRoman(SFG_Fonts^ pFonts);
+	static void CreateFontStrokeRoman(SFG_Fonts^ pFonts);
 };
 
