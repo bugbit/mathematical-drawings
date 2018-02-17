090ce226ef29f0764d3e51dcca3c95b4a82d3e12
bugbit <ohernandezba@gmail.com>
2018-02-17 01:47:24 +0100

    Dibuix Aboit It

--- OpenTK/Dibuixos/Dibuixos/Dibuixos/Core/GLUtil.cs
+++ OpenTK/Dibuixos/Dibuixos/Dibuixos/Core/GLUtil.cs
@@ -4,27 +4,27 @@ using OpenTKGLUT;
 using System.Linq;
 
 namespace Dibuixos.Core
-{
+{    
     public static class GLUtil
     {
-        public static void OrthoW()
-        {
-            var pGameWindow = Dibuixos.DibuixosMain.GameWindow;
+        //public static void OrthoW()
+        //{
+        //    var pGameWindow = Dibuixos.DibuixosMain.GameWindow;
 
-            GL.LoadIdentity();
-            GL.Ortho(0, pGameWindow.Width, 0, pGameWindow.Height, -1, 1);
-        }
+        //    GL.LoadIdentity();
+        //    GL.Ortho(0, pGameWindow.Width, 0, pGameWindow.Height, -1, 1);
+        //}
 
-        public static void OrthoWindow()
-        {
-            //Initialize Projection Matrix
-            GL.MatrixMode(MatrixMode.Projection);
-            OrthoW();
+        //public static void OrthoWindow()
+        //{
+        //    //Initialize Projection Matrix
+        //    GL.MatrixMode(MatrixMode.Projection);
+        //    OrthoW();
 
-            //Initialize Modelview Matrix
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadIdentity();
-        }
+        //    //Initialize Modelview Matrix
+        //    GL.MatrixMode(MatrixMode.Modelview);
+        //    GL.LoadIdentity();
+        //}
 
         public static float StrokeHeight(int fontID, string str)
         {
