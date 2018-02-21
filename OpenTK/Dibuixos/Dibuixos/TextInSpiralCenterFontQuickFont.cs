c99bdeb588e9e22d326f309f46392ffbf16a7063
bugbit <ohernandezba@gmail.com>
2018-02-21 15:41:24 +0100

    Text in spiral Center

---
+++ OpenTK/Dibuixos/Dibuixos/TextInSpiralCenterFontQuickFont.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using QuickFont;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dibuixos
+{
+    public class TextInSpiralCenterFontQuickFont : GameWindow
+    {
+        private Matrix4 mProjectionMatrix;
+        private QFont mFont;
+
+        /// <summary>
+        /// Called when your window is resized. Set your viewport here. It is also
+        /// a good place to set up your projection matrix (which probably changes
+        /// along when the aspect ratio of your window).
+        /// </summary>
+        /// <param name="e">Not used.</param>
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            mProjectionMatrix = Matrix4.CreateOrthographicOffCenter(ClientRectangle.X, ClientRectangle.Width, ClientRectangle.Y, ClientRectangle.Height, -1.0f, 1.0f);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            mFont = new QFont("Fonts/HappySans.ttf", new QuickFont.Configuration.QFontBuilderConfiguration());
+        }
+    }
+}
